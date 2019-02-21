fe4a93318754b64b9ccff8a3899bd329ab65444c
rafatili <rafaelchiea@gmail.com>
2019-02-21 01:30:07 -0300

    qlt test bug really fixed now. analysis script OK.

--- Results Analysis/analysis.m
+++ Results Analysis/analysis.m
@@ -15,6 +15,7 @@ if iscell(fnames)
     Lfiles = length(fnames);
 else
     Lfiles = 1;
+    fnames = {fnames};
 end
 
 maxcols = 3; %max. number of subplot columns
@@ -30,19 +31,15 @@ aWF = zeros(Lfiles,5); %considering 5 snr values
 aEnv = zeros(Lfiles,5);
 
 for ii = 1:Lfiles;
-    % conditions to deal with the single or multiple files selection
-    if iscell(fnames)
-        filename = fnames{ii};
-    else
-        filename = fnames;
-    end
+
+    filename = fnames{ii};
     %
     load(filename)
     
     if isfield(resultados,'snr')
         snr = resultados.snr;
     else
-        snr = -15:-5:-35; %on the first simulation this info was not saved 
+        snr = -15:-5:-35; %on the first simulation this info was not saved
     end
     
     L = length(snr);
@@ -95,7 +92,7 @@ for ii = 1:Lfiles;
     
     ax = gca;
     ax.XLim = ax.XLim + [-1,+1]*5;
-    ttl = sprintf('%s', filename(1:end-8));
+    ttl = sprintf('S%d',ii); %('%s', filename(1:end-8));
     title(ttl)
     
     hold on;
@@ -125,49 +122,155 @@ for ii = 1:Lfiles;
     ylabel('Correct words(%)');
     xlabel('SNR [dB]');
     legend({'WF', 'EnvEst.'},'Location','northwest');
-    ttl = sprintf('%s', filename(1:end-8));
+    ttl = sprintf('S%d',ii); %('%s', filename(1:end-8));
     title(ttl)
     ylim([-10,110])
 end
 
-bp2 = figure; %figure for boxplot2
-%reorganize the rate matrix
-[row,col] = size(aWF);
-rate2 = zeros(row,col+L);
-dummycol = nan*ones(row,1);
-
-X1 = [0 1 2];
-Xpos = zeros(1,3*L);
-for jj = 0:L-1;
-    rate2(:,jj*3+1) = aWF(:,L-jj);
-    rate2(:,jj*3+2) = dummycol;
-    rate2(:,jj*3+3) = aEnv(:,L-jj);
+%% plots for all subjects together
+if Lfiles > 1
+    %% plot 1
+    bp2 = figure; %figure for boxplot2
+    %reorganize the rate matrix
+    [row,col] = size(aWF);
+    rate2 = zeros(row,col+L);
+    dummycol = nan*ones(row,1);
     
-    Xpos(jj*3+(1:3)) = X1 + jj*10;
+    X1 = [0 1 2];
+    Xpos = zeros(1,3*L);
+    for jj = 0:L-1;
+        rate2(:,jj*3+1) = aWF(:,L-jj);
+        rate2(:,jj*3+2) = dummycol;
+        rate2(:,jj*3+3) = aEnv(:,L-jj);
+        
+        Xpos(jj*3+(1:3)) = X1 + jj*10;
+    end
+    
+    %boxplot
+    figure(bp2)
+    
+    c0 = colormap(lines(2));
+    c = [c0(1,:); ones(1,3); c0(2,:)];
+    C = repmat(c,L,1);  % this is the trick for coloring the boxes
+    % regular plot
+    boxplot(rate2*100, 'positions', Xpos, 'colors', C, 'plotstyle', 'compact', ...
+        'labels', labelo, 'LabelOrientation','horizontal'); % label only two categories
+    
+    ax = gca;
+    ax.XLim = ax.XLim + [-1,+1]*5;
+    ttl = 'All subjects';
+    title(ttl)
+    
+    hold on;
+    for jj = 1:2
+        plot(NaN,1,'color', c0(jj,:), 'LineWidth', 4);
+    end
+    
+    ylabel('Correct words(%)');
+    xlabel('SNR [dB]');
+    
+    legend({'WF', 'EnvEst.'},'Location','northwest');
+    ylim([-10,110])
+    
+    %% plot 2
+    bp3 = figure; %figure for boxplot2
+    %reorganize the rate matrix
+    [row,col] = size(aWF);
+    
+    delta = aEnv - aWF;
+    delta = fliplr(delta);
+    %boxplot
+    figure(bp3)
+   
+    % regular plot
+    boxplot(delta*100, 'plotstyle', 'compact', ...
+        'labels', fliplr(snr), 'LabelOrientation','horizontal'); % label only two categories
+    
+    ttl = 'All subjects';
+    title(ttl)
+    
+    ylabel('$\Delta$ Correct words [\%] (Env - WF))', 'interpreter', 'Latex');
+    xlabel('SNR [dB]');
+    
+    ylim([-10,110])
+end
+%% quality test
+
+% there are 3 combinations for this test, 20 sentences for each
+Qs = zeros(20,Lfiles,3); % comb 1
+
+for ii = 1:Lfiles
+    aux = fnames{ii};
+    filename = sprintf('%s_QLT.mat', aux(1:end-8));
+    
+    if exist(filename, 'file')
+        load(filename)
+    
+        SNRqlt = -10; %dB
+        
+        Qs(:,ii,1) = Qresultados.resultados(:,1);
+        Qs(:,ii,2) = Qresultados.resultados(:,2);
+        Qs(:,ii,3) = Qresultados.resultados(:,3);
+    else
+        Qs(:,ii,1) = NaN*ones(20,1);
+        Qs(:,ii,2) = NaN*ones(20,1);
+        Qs(:,ii,3) = NaN*ones(20,1);
+    end
 end
 
-%boxplot
-figure(bp2)
+qf(1) = figure;
+qf(2) = figure;
+qf(3) = figure;
 
-c0 = colormap(lines(2));
-c = [c0(1,:); ones(1,3); c0(2,:)];
-C = repmat(c,L,1);  % this is the trick for coloring the boxes
-% regular plot
-boxplot(rate2*100, 'positions', Xpos, 'colors', C, 'plotstyle', 'compact', ...
-    'labels', labelo, 'LabelOrientation','horizontal'); % label only two categories
-
-ax = gca;
-ax.XLim = ax.XLim + [-1,+1]*5;
-ttl = 'All subjects';
-title(ttl)
-
-hold on;
-for jj = 1:2
-    plot(NaN,1,'color', c0(jj,:), 'LineWidth', 4);
+for ii = 1:3
+    M = Qs(:,:,ii);
+    nancols = all( isnan( M ), 1 );
+    M( :, nancols) = []; %excludes participants that did not take the quality test
+    subjectidx = 1:Lfiles;
+    subjectidx(nancols) = [];
+    
+    figure(qf(ii))
+    
+    [~, Mfiles] = size(M);
+    
+    subplot(1,2,1)
+    data = M(1:10,:);
+    data = [data(:);data(:)];
+    
+    grp = zeros(2*10*Mfiles,1);
+    labels = cell(Mfiles+1,1);
+    for jj = 1:Mfiles
+        grp((jj-1)*10+1:(jj-1)*10+10) = (jj-1)*ones(10,1);
+        labels{jj} = sprintf('S%d', subjectidx(jj));
+    end
+    grp(10*Mfiles+1:end) = Mfiles;
+    labels{end} = 'All';
+    
+    boxplot(data, grp, 'plotstyle', 'compact','labels', labels,'LabelOrientation','horizontal'); % label only two categories
+    title(sprintf('%s with respect to %s', Qresultados.comb{ii,2}, Qresultados.comb{ii,1}))
+    
+    xlabel('Subjects')
+    ylabel('Quality evaluation')
+    ylim([-5,+5])
+    
+    %% test repetition data
+    subplot(1,2,2)
+    data = M(11:end,:);
+    data = [data(:);data(:)];
+    
+    grp = zeros(2*10*Mfiles,1);
+    labels = cell(Mfiles+1,1);
+    for jj = 1:Mfiles
+        grp((jj-1)*10+1:(jj-1)*10+10) = (jj-1)*ones(10,1);
+        labels{jj} = sprintf('S%d', subjectidx(jj));
+    end
+    grp(10*Mfiles+1:end) = Mfiles;
+    labels{end} = 'All';
+    
+    boxplot(data, grp, 'plotstyle', 'compact','labels', labels,'LabelOrientation','horizontal'); % label only two categories
+    title(sprintf('%s with respect to %s', Qresultados.comb{ii,2}, Qresultados.comb{ii,1}))
+    
+    xlabel('Subjects')
+    ylabel('Quality evaluation')
+    ylim([-5,+5])
 end
-
-ylabel('Correct words(%)');
-xlabel('SNR [dB]');
-
-legend({'WF', 'EnvEst.'},'Location','northwest');
-ylim([-10,110])
